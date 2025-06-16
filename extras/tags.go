091f807f7219ef44441026e0fb4736b6fca2edb0
Josh <47613996+jokelyo@users.noreply.github.com>
2025-06-15 23:36:16 -0500

    refactor: update extras.tag fields and add remaining CRUD methods (#7)

--- extras/tags.go
+++ extras/tags.go
@@ -1,74 +1,77 @@
 package extras
 
 import (
-	"errors"
-	"fmt"
-	"github.com/josh-silvas/gonautobot/core"
-	"github.com/josh-silvas/gonautobot/shared"
-	"net/http"
 	"net/url"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/josh-silvas/gonautobot/core"
+)
+
+const (
+	// extrasEndpointTag : Endpoint for interacting with Tags in Nautobot.
+	extrasEndpointTag = "extras/tags/"
 )
 
 type (
 	// Tag : Data type entry for a Tag in Nautobot.
 	Tag struct {
-		ID          string `json:"id"`
-		Color       string `json:"color"`
-		Created     string `json:"created"`
-		Display     string `json:"display"`
-		LastUpdated string `json:"last_updated"`
-		Name        string `json:"name"`
-		Slug        string `json:"slug"`
-		URL         string `json:"url"`
+		ID           uuid.UUID      `json:"id"`
+		Color        string         `json:"color"`
+		ContentTypes []string       `json:"content_types"`
+		Created      time.Time      `json:"created"`
+		CustomFields map[string]any `json:"custom_fields"`
+		Description  string         `json:"description"`
+		Display      string         `json:"display"`
+		LastUpdated  time.Time      `json:"last_updated"`
+		Name         string         `json:"name"`
+		NaturalSlug  string         `json:"natural_slug"`
+		NotesURL     string         `json:"notes_url"`
+		ObjectType   string         `json:"object_type"`
+		TaggedItems  int            `json:"tagged_items"`
+		URL          string         `json:"url"`
+	}
+
+	// NewTag : Data type for creating a new Tag in Nautobot.
+	NewTag struct {
+		Name          string         `json:"name"`
+		ContentTypes  []string       `json:"content_types"`
+		Color         string         `json:"color,omitempty"`
+		CustomFields  map[string]any `json:"custom_fields,omitempty"`
+		Description   string         `json:"description,omitempty"`
+		Relationships map[string]any `json:"relationships,omitempty"`
 	}
 )
 
 // TagGet : Fetches a tag from Nautobot by the slug name.
-func (c *Client) TagGet(uuid string) (Tag, error) {
-	req, err := c.Request(http.MethodGet, fmt.Sprintf("extras/tags/%s/", url.PathEscape(uuid)), nil, nil)
-	if err != nil {
-		return Tag{}, err
-	}
-
-	ret := new(Tag)
-	if err = c.UnmarshalDo(req, ret); err != nil {
-		return Tag{}, err
-	}
-
-	return *ret, nil
+func (c *Client) TagGet(id uuid.UUID) (*Tag, error) {
+	return core.Get[Tag](c.Client, extrasEndpointTag, id)
 }
 
 // TagFilter : Returns an array of tags from Nautobot filtered by the q (query values)
 func (c *Client) TagFilter(q *url.Values) ([]Tag, error) {
 	resp := make([]Tag, 0)
-	if err := core.Paginate[Tag](c.Client, "extras/tags/", q, &resp); err != nil {
-		return nil, err
-	}
-	return resp, nil
+	return resp, core.Paginate[Tag](c.Client, extrasEndpointTag, q, &resp)
+}
+
+// TagAll : Returns an array of all tags from Nautobot
+func (c *Client) TagAll() ([]Tag, error) {
+	resp := make([]Tag, 0)
+	return resp, core.Paginate[Tag](c.Client, extrasEndpointTag, nil, &resp)
 }
 
 // TagCreate : Generate a new Tag in Nautobot.
-func (c *Client) TagCreate(tag Tag) (Tag, error) {
-	req, err := c.Request(http.MethodPost, "extras/tags/", tag, nil)
-	if err != nil {
-		return tag, err
-	}
-
-	var r Tag
-	if err := c.UnmarshalDo(req, &r); err != nil {
-		return tag, fmt.Errorf("TagCreate.error.UnmarshalDo(%w)", err)
-	}
-	return r, nil
+func (c *Client) TagCreate(tag NewTag) (*Tag, error) {
+	return core.Create[Tag, NewTag](c.Client, extrasEndpointTag, tag)
 }
 
 // TagDelete : Extras method to delete a tag by UUIDv4 identifier.
-func (c *Client) TagDelete(uuid string) error {
-	if uuid == "" {
-		return errors.New("TagDelete.error.UUID(UUIDv4 is missing)")
-	}
-	req, err := c.Request(http.MethodDelete, "extras/tags/", shared.BulkDelete{{ID: uuid}}, nil)
-	if err != nil {
-		return err
-	}
-	return c.UnmarshalDo(req, nil)
+func (c *Client) TagDelete(id uuid.UUID) error {
+	return core.Delete(c.Client, extrasEndpointTag, id)
+}
+
+// TagUpdate : Update an existing Tag in Nautobot.
+func (c *Client) TagUpdate(id uuid.UUID, patch map[string]any) (*Tag, error) {
+	return core.Update[Tag](c.Client, extrasEndpointTag, id, patch)
 }
