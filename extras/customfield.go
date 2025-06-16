5fb24438cdc0e3090f795885340593970a92342e
Josh <47613996+jokelyo@users.noreply.github.com>
2025-06-15 23:37:52 -0500

    feat: add extras.custom-field support (#10)

---
+++ extras/customfield.go
@@ -0,0 +1,92 @@
+package extras
+
+import (
+	"net/url"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/josh-silvas/gonautobot/core"
+	"github.com/josh-silvas/gonautobot/shared"
+)
+
+const (
+	extrasEndpointCustomField = "extras/custom-fields/"
+)
+
+type (
+	// CustomField : Represents a custom field in Nautobot.
+	CustomField struct {
+		ID              uuid.UUID         `json:"id"`
+		AdvancedAI      bool              `json:"advanced_ai"`
+		ContentTypes    []string          `json:"content_types"`
+		Created         time.Time         `json:"created"`
+		Default         any               `json:"default"`
+		Description     string            `json:"description"`
+		Display         string            `json:"display"`
+		FilterLogic     shared.LabelValue `json:"filter_logic"`
+		Grouping        string            `json:"grouping"`
+		Key             string            `json:"key"`
+		Label           string            `json:"label"`
+		LastUpdated     time.Time         `json:"last_updated"`
+		NaturalSlug     string            `json:"natural_slug"`
+		NotesURL        string            `json:"notes_url"`
+		ObjectType      string            `json:"object_type"`
+		Required        bool              `json:"required"`
+		Type            shared.LabelValue `json:"type"`
+		URL             string            `json:"url"`
+		ValidationMax   *int              `json:"validation_maximum"`
+		ValidationMin   *int              `json:"validation_minimum"`
+		ValidationRegex string            `json:"validation_regex"`
+		Weight          int               `json:"weight"`
+	}
+
+	// NewCustomField : Represents a new custom field to be created in Nautobot.
+	NewCustomField struct {
+		ContentTypes    []string `json:"content_types"`
+		FilterLogic     string   `json:"filter_logic"`
+		Key             string   `json:"key"`
+		Label           string   `json:"label"`
+		Type            string   `json:"type"`
+		AdvancedAI      bool     `json:"advanced_ai,omitempty"`
+		Default         any      `json:"default,omitempty"`
+		Description     string   `json:"description,omitempty"`
+		Grouping        string   `json:"grouping,omitempty"`
+		Required        bool     `json:"required,omitempty"`
+		ValidationMin   *int     `json:"validation_minimum,omitempty"`
+		ValidationMax   *int     `json:"validation_maximum,omitempty"`
+		ValidationRegex string   `json:"validation_regex,omitempty"`
+		Weight          int      `json:"weight,omitempty"`
+	}
+)
+
+// CustomFieldGet : Get a CustomField by UUID identifier.
+func (c *Client) CustomFieldGet(id uuid.UUID) (*CustomField, error) {
+	return core.Get[CustomField](c.Client, extrasEndpointCustomField, id)
+}
+
+// CustomFieldFilter : Get a list of CustomFields based on query parameters.
+func (c *Client) CustomFieldFilter(q *url.Values) ([]CustomField, error) {
+	customFields := make([]CustomField, 0)
+	return customFields, core.Paginate[CustomField](c.Client, extrasEndpointCustomField, q, &customFields)
+}
+
+// CustomFieldAll : Get all CustomFields in Nautobot.
+func (c *Client) CustomFieldAll() ([]CustomField, error) {
+	customFields := make([]CustomField, 0)
+	return customFields, core.Paginate[CustomField](c.Client, extrasEndpointCustomField, nil, &customFields)
+}
+
+// CustomFieldCreate : Generate a new CustomField record in Nautobot.
+func (c *Client) CustomFieldCreate(obj NewCustomField) (*CustomField, error) {
+	return core.Create[CustomField, NewCustomField](c.Client, extrasEndpointCustomField, obj)
+}
+
+// CustomFieldDelete : Delete a CustomField by UUID identifier.
+func (c *Client) CustomFieldDelete(id uuid.UUID) error {
+	return core.Delete(c.Client, extrasEndpointCustomField, id)
+}
+
+// CustomFieldUpdate : Update an existing CustomField record in Nautobot.
+func (c *Client) CustomFieldUpdate(id uuid.UUID, patch map[string]any) (*CustomField, error) {
+	return core.Update[CustomField](c.Client, extrasEndpointCustomField, id, patch)
+}
---
+++ tests/extras_customfield_test.go
@@ -0,0 +1,118 @@
+package nautobot_test
+
+import (
+	"net/url"
+	"path"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/josh-silvas/gonautobot/extras"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/h2non/gock.v1"
+)
+
+const (
+	testCustomFieldID = "b6e143ff-c980-4c1f-a2b7-70a59042a188"
+)
+
+func TestClient_CustomFieldGet(t *testing.T) {
+	defer gock.Off()
+	gock.New(testURL).Get("extras/custom-fields/" + testCustomFieldID + "/").Reply(200).
+		File(path.Join("fixtures", "extras", "customfield_200_1.json"))
+
+	id, err := uuid.Parse(testCustomFieldID)
+	require.NoError(t, err)
+
+	resp, err := testClient.Extras.CustomFieldGet(id)
+	require.NoError(t, err)
+	assert.Equal(t, "Terraform", resp.Label)
+	assert.False(t, resp.Default.(bool))
+	assert.Equal(t, id, resp.ID)
+}
+
+func TestClient_CustomFieldFilter(t *testing.T) {
+	defer gock.Off()
+	gock.New(testURL).Get("extras/custom-fields/").Reply(200).
+		File(path.Join("fixtures", "extras", "customfields_200_1.json"))
+
+	q := &url.Values{}
+
+	resp, err := testClient.Extras.CustomFieldFilter(q)
+	require.NoError(t, err)
+	assert.Len(t, resp, 3)
+	assert.Equal(t, resp[2].ID, uuid.MustParse(testCustomFieldID))
+}
+
+func TestClient_CustomFieldAll(t *testing.T) {
+	defer gock.Off()
+	gock.New(testURL).Get("extras/custom-fields/").Reply(200).
+		File(path.Join("fixtures", "extras", "customfields_200_1.json"))
+
+	resp, err := testClient.Extras.CustomFieldAll()
+	require.NoError(t, err)
+
+	assert.Len(t, resp, 3)
+	assert.Equal(t, "Custom Thing1", resp[0].Label)
+	assert.Equal(t, "Terraform", resp[2].Label)
+	// check Default fields
+	assert.Nil(t, resp[0].Default)
+	assert.Equal(t, map[string]any{"stuff": "things"}, resp[1].Default.(map[string]any))
+	assert.False(t, resp[2].Default.(bool))
+}
+
+func TestClient_CustomFieldCreate(t *testing.T) {
+	defer gock.Off()
+	newCf := extras.NewCustomField{
+		ContentTypes: []string{"dcim.device", "dcim.location", "dcim.locationtype", "tenancy.tenant"},
+		Key:          "terraform",
+		Label:        "Terraform",
+		Type:         "boolean",
+	}
+
+	gock.New(testURL).Post("extras/custom-fields/").
+		JSON(newCf).
+		Reply(201).
+		File(path.Join("fixtures", "extras", "customfield_create_200_1.json"))
+
+	resp, err := testClient.Extras.CustomFieldCreate(newCf)
+	require.NoError(t, err)
+
+	assert.Equal(t, "Terraform", resp.Label)
+	assert.Equal(t, "terraform", resp.Key)
+	assert.Equal(t, uuid.MustParse(testCustomFieldID), resp.ID)
+}
+
+func TestClient_CustomFieldUpdate(t *testing.T) {
+	defer gock.Off()
+	updateData := map[string]any{
+		"description": "updated description",
+		"weight":      250,
+	}
+
+	gock.New(testURL).Patch("extras/custom-fields/" + testCustomFieldID + "/").
+		JSON(updateData).
+		Reply(200).
+		File(path.Join("fixtures", "extras", "customfield_update_200_1.json"))
+
+	id, err := uuid.Parse(testCustomFieldID)
+	require.NoError(t, err)
+
+	resp, err := testClient.Extras.CustomFieldUpdate(id, updateData)
+	require.NoError(t, err)
+
+	assert.Equal(t, "updated description", resp.Description)
+	assert.Equal(t, 250, resp.Weight)
+	assert.Equal(t, id, resp.ID)
+}
+
+func TestClient_CustomFieldDelete(t *testing.T) {
+	defer gock.Off()
+	gock.New(testURL).Delete("extras/custom-fields/" + testCustomFieldID + "/").Reply(204)
+
+	id, err := uuid.Parse(testCustomFieldID)
+	require.NoError(t, err)
+
+	err = testClient.Extras.CustomFieldDelete(id)
+	require.NoError(t, err)
+}
