74764e00581acf4ba81dfa54148cb560f63e7848
Josh <47613996+jokelyo@users.noreply.github.com>
2025-06-17 02:01:57 -0500

    feat: add dcim.device-families support (#15)

---
+++ dcim/devicefamily.go
@@ -0,0 +1,72 @@
+package dcim
+
+import (
+	"net/url"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/josh-silvas/gonautobot/core"
+	"github.com/josh-silvas/gonautobot/extras"
+)
+
+const (
+	dcimEndpointDeviceFamily = "dcim/device-families/"
+)
+
+type (
+	// DeviceFamily represents a device family in Nautobot's DCIM module.
+	DeviceFamily struct {
+		ID           uuid.UUID      `json:"id"`
+		Created      time.Time      `json:"created"`
+		CustomFields map[string]any `json:"custom_fields"`
+		Description  string         `json:"description"`
+		Display      string         `json:"display"`
+		LastUpdated  time.Time      `json:"last_updated"`
+		Name         string         `json:"name"`
+		NaturalSlug  string         `json:"natural_slug"`
+		NotesURL     string         `json:"notes_url"`
+		ObjectType   string         `json:"object_type"`
+		Tags         []extras.Tag   `json:"tags"`
+		URL          string         `json:"url"`
+	}
+
+	// NewDeviceFamily represents the data required to create a new device family.
+	NewDeviceFamily struct {
+		Name         string         `json:"name"`
+		CustomFields map[string]any `json:"custom_fields,omitempty"`
+		Description  string         `json:"description,omitempty"`
+		Tags         []string       `json:"tags,omitempty"`
+	}
+)
+
+// DeviceFamilyGet : Get a DeviceFamily by UUID identifier.
+func (c *Client) DeviceFamilyGet(id uuid.UUID) (*DeviceFamily, error) {
+	return core.Get[DeviceFamily](c.Client, dcimEndpointDeviceFamily, id)
+}
+
+// DeviceFamilyFilter : Get a list of DeviceFamilies based on query parameters.
+func (c *Client) DeviceFamilyFilter(q *url.Values) ([]DeviceFamily, error) {
+	deviceFamilies := make([]DeviceFamily, 0)
+	return deviceFamilies, core.Paginate[DeviceFamily](c.Client, dcimEndpointDeviceFamily, q, &deviceFamilies)
+}
+
+// DeviceFamilyAll : Get all DeviceFamilies in Nautobot.
+func (c *Client) DeviceFamilyAll() ([]DeviceFamily, error) {
+	deviceFamilies := make([]DeviceFamily, 0)
+	return deviceFamilies, core.Paginate[DeviceFamily](c.Client, dcimEndpointDeviceFamily, nil, &deviceFamilies)
+}
+
+// DeviceFamilyCreate : Generate a new DeviceFamily record in Nautobot.
+func (c *Client) DeviceFamilyCreate(obj NewDeviceFamily) (*DeviceFamily, error) {
+	return core.Create[DeviceFamily, NewDeviceFamily](c.Client, dcimEndpointDeviceFamily, obj)
+}
+
+// DeviceFamilyDelete : Delete a DeviceFamily by UUID identifier.
+func (c *Client) DeviceFamilyDelete(id uuid.UUID) error {
+	return core.Delete(c.Client, dcimEndpointDeviceFamily, id)
+}
+
+// DeviceFamilyUpdate : Update an existing DeviceFamily record in Nautobot.
+func (c *Client) DeviceFamilyUpdate(id uuid.UUID, patch map[string]any) (*DeviceFamily, error) {
+	return core.Update[DeviceFamily](c.Client, dcimEndpointDeviceFamily, id, patch)
+}
---
+++ tests/dcim_devicefamily_test.go
@@ -0,0 +1,114 @@
+package nautobot_test
+
+import (
+	"net/url"
+	"path"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/josh-silvas/gonautobot/dcim"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/h2non/gock.v1"
+)
+
+const (
+	testDeviceFamilyID = "8985db57-2dcd-4bce-8129-5f86e3a65174"
+)
+
+func TestClient_DeviceFamilyGet(t *testing.T) {
+	defer gock.Off()
+	gock.New(testURL).Get("dcim/device-families/" + testDeviceFamilyID + "/").Reply(200).
+		File(path.Join("fixtures", "dcim", "devicefamily_200_1.json"))
+
+	id, err := uuid.Parse(testDeviceFamilyID)
+	require.NoError(t, err)
+
+	resp, err := testClient.Dcim.DeviceFamilyGet(id)
+	require.NoError(t, err)
+	assert.Equal(t, "Family1", resp.Name)
+	assert.Equal(t, id, resp.ID)
+	assert.True(t, gock.IsDone())
+}
+
+func TestClient_DeviceFamilyFilter(t *testing.T) {
+	defer gock.Off()
+	gock.New(testURL).Get("dcim/device-families/").MatchParam("name", "Family1").Reply(200).
+		File(path.Join("fixtures", "dcim", "devicefamilies_200_1.json"))
+
+	q := &url.Values{}
+	q.Set("name", "Family1")
+
+	resp, err := testClient.Dcim.DeviceFamilyFilter(q)
+	require.NoError(t, err)
+	assert.Len(t, resp, 2)
+	assert.Equal(t, "Family1", resp[0].Name)
+	assert.True(t, gock.IsDone())
+}
+
+func TestClient_DeviceFamilyAll(t *testing.T) {
+	defer gock.Off()
+	gock.New(testURL).Get("dcim/device-families/").Reply(200).
+		File(path.Join("fixtures", "dcim", "devicefamilies_200_1.json"))
+
+	resp, err := testClient.Dcim.DeviceFamilyAll()
+	require.NoError(t, err)
+
+	assert.Len(t, resp, 2)
+	assert.Equal(t, "Family1", resp[0].Name)
+	assert.True(t, gock.IsDone())
+}
+
+func TestClient_DeviceFamilyCreate(t *testing.T) {
+	defer gock.Off()
+	newDeviceFamily := dcim.NewDeviceFamily{
+		Name:        "Family1",
+		Description: "A family for devices",
+	}
+
+	gock.New(testURL).Post("dcim/device-families/").
+		JSON(newDeviceFamily).
+		Reply(201).
+		File(path.Join("fixtures", "dcim", "devicefamily_create_201_1.json"))
+
+	resp, err := testClient.Dcim.DeviceFamilyCreate(newDeviceFamily)
+	require.NoError(t, err)
+	assert.Equal(t, newDeviceFamily.Name, resp.Name)
+	assert.Equal(t, newDeviceFamily.Description, resp.Description)
+	assert.Equal(t, uuid.MustParse(testDeviceFamilyID), resp.ID)
+	assert.True(t, gock.IsDone())
+}
+
+func TestClient_DeviceFamilyUpdate(t *testing.T) {
+	defer gock.Off()
+	updateData := map[string]any{
+		"description": "updated description",
+	}
+
+	gock.New(testURL).Patch("dcim/device-families/" + testDeviceFamilyID + "/").
+		JSON(updateData).
+		Reply(200).
+		File(path.Join("fixtures", "dcim", "devicefamily_update_200_1.json"))
+
+	id, err := uuid.Parse(testDeviceFamilyID)
+	require.NoError(t, err)
+
+	resp, err := testClient.Dcim.DeviceFamilyUpdate(id, updateData)
+	require.NoError(t, err)
+
+	assert.Equal(t, "updated description", resp.Description)
+	assert.Equal(t, id, resp.ID)
+	assert.True(t, gock.IsDone())
+}
+
+func TestClient_DeviceFamilyDelete(t *testing.T) {
+	defer gock.Off()
+	gock.New(testURL).Delete("dcim/device-families/" + testDeviceFamilyID + "/").Reply(204)
+
+	id, err := uuid.Parse(testDeviceFamilyID)
+	require.NoError(t, err)
+
+	err = testClient.Dcim.DeviceFamilyDelete(id)
+	require.NoError(t, err)
+	assert.True(t, gock.IsDone())
+}
