b0ff39e8467199f1574536ce21362e50362a67e9
Josh <47613996+jokelyo@users.noreply.github.com>
2025-06-18 23:54:25 -0500

    feat: add dcim.software-versions support (#17)

---
+++ dcim/softwareversion.go
@@ -0,0 +1,86 @@
+package dcim
+
+import (
+	"net/url"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/josh-silvas/gonautobot/core"
+	"github.com/josh-silvas/gonautobot/extras"
+)
+
+const (
+	dcimEndpointSoftwareVersion = "dcim/software-versions/"
+)
+
+type (
+	// SoftwareVersion represents a software version in Nautobot.
+	SoftwareVersion struct {
+		ID               uuid.UUID      `json:"id"`
+		Alias            string         `json:"alias"`
+		Created          time.Time      `json:"created"`
+		CustomFields     map[string]any `json:"custom_fields"`
+		Display          string         `json:"display"`
+		DocumentationURL string         `json:"documentation_url"`
+		EndOfSupportDate *string        `json:"end_of_support_date"`
+		LastUpdated      time.Time      `json:"last_updated"`
+		LongTermSupport  bool           `json:"long_term_support"`
+		NaturalSlug      string         `json:"natural_slug"`
+		NotesURL         string         `json:"notes_url"`
+		ObjectType       string         `json:"object_type"`
+		Platform         Platform       `json:"platform"`
+		PreRelease       bool           `json:"pre_release"`
+		ReleaseDate      *string        `json:"release_date"`
+		Status           extras.Status  `json:"status"`
+		Tags             []extras.Tag   `json:"tags"`
+		URL              string         `json:"url"`
+		Version          string         `json:"version"`
+	}
+
+	// NewSoftwareVersion represents the data required to create a new software version.
+	NewSoftwareVersion struct {
+		Platform         string         `json:"platform"`
+		Status           string         `json:"status"`
+		Version          string         `json:"version"`
+		Alias            string         `json:"alias,omitempty"`
+		CustomFields     map[string]any `json:"custom_fields,omitempty"`
+		DocumentationURL string         `json:"documentation_url,omitempty"`
+		EndOfSupportDate string         `json:"end_of_support_date,omitempty"`
+		LongTermSupport  bool           `json:"long_term_support"`
+		PreRelease       bool           `json:"pre_release"`
+		ReleaseDate      string         `json:"release_date,omitempty"`
+		Tags             []string       `json:"tags,omitempty"`
+	}
+)
+
+// SoftwareVersionGet : Get a SoftwareVersion by UUID identifier.
+func (c *Client) SoftwareVersionGet(id uuid.UUID) (*SoftwareVersion, error) {
+	return core.Get[SoftwareVersion](c.Client, dcimEndpointSoftwareVersion, id)
+}
+
+// SoftwareVersionFilter : Get a list of SoftwareVersions based on query parameters.
+func (c *Client) SoftwareVersionFilter(q *url.Values) ([]SoftwareVersion, error) {
+	softwareVersions := make([]SoftwareVersion, 0)
+	return softwareVersions, core.Paginate[SoftwareVersion](c.Client, dcimEndpointSoftwareVersion, q, &softwareVersions)
+}
+
+// SoftwareVersionAll : Get all SoftwareVersions in Nautobot.
+func (c *Client) SoftwareVersionAll() ([]SoftwareVersion, error) {
+	softwareVersions := make([]SoftwareVersion, 0)
+	return softwareVersions, core.Paginate[SoftwareVersion](c.Client, dcimEndpointSoftwareVersion, nil, &softwareVersions)
+}
+
+// SoftwareVersionCreate : Generate a new SoftwareVersion record in Nautobot.
+func (c *Client) SoftwareVersionCreate(obj NewSoftwareVersion) (*SoftwareVersion, error) {
+	return core.Create[SoftwareVersion, NewSoftwareVersion](c.Client, dcimEndpointSoftwareVersion, obj)
+}
+
+// SoftwareVersionDelete : Delete a SoftwareVersion by UUID identifier.
+func (c *Client) SoftwareVersionDelete(id uuid.UUID) error {
+	return core.Delete(c.Client, dcimEndpointSoftwareVersion, id)
+}
+
+// SoftwareVersionUpdate : Update an existing SoftwareVersion record in Nautobot.
+func (c *Client) SoftwareVersionUpdate(id uuid.UUID, patch map[string]any) (*SoftwareVersion, error) {
+	return core.Update[SoftwareVersion](c.Client, dcimEndpointSoftwareVersion, id, patch)
+}
---
+++ tests/dcim_software_version_test.go
@@ -0,0 +1,103 @@
+package nautobot_test
+
+import (
+	"net/url"
+	"path"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/josh-silvas/gonautobot/dcim"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/h2non/gock.v1"
+)
+
+const (
+	testSoftwareVersionID = "bd0d3850-385d-47c5-8117-75de4c92d6c6"
+)
+
+func TestClient_SoftwareVersionGet(t *testing.T) {
+	gock.New(testURL).Get("dcim/software-versions/" + testSoftwareVersionID + "/").Reply(200).
+		File(path.Join("fixtures", "dcim", "softwareversion_200_1.json"))
+
+	id, err := uuid.Parse(testSoftwareVersionID)
+	require.NoError(t, err)
+
+	resp, err := testClient.Dcim.SoftwareVersionGet(id)
+	require.NoError(t, err)
+	assert.Equal(t, "6.15.2", resp.Version)
+	assert.Equal(t, id, resp.ID)
+}
+
+func TestClient_SoftwareVersionFilter(t *testing.T) {
+	gock.New(testURL).Get("dcim/software-versions/").Reply(200).
+		File(path.Join("fixtures", "dcim", "softwareversions_200_1.json"))
+
+	q := &url.Values{}
+	q.Set("status", "active")
+
+	resp, err := testClient.Dcim.SoftwareVersionFilter(q)
+	require.NoError(t, err)
+	assert.Len(t, resp, 2)
+	assert.Equal(t, "4.34.0F", resp[0].Version)
+}
+
+func TestClient_SoftwareVersionAll(t *testing.T) {
+	gock.New(testURL).Get("dcim/software-versions/").Reply(200).
+		File(path.Join("fixtures", "dcim", "softwareversions_200_1.json"))
+
+	resp, err := testClient.Dcim.SoftwareVersionAll()
+	require.NoError(t, err)
+
+	assert.Len(t, resp, 2)
+	assert.Equal(t, "4.34.0F", resp[0].Version)
+}
+
+func TestClient_SoftwareVersionCreate(t *testing.T) {
+	newSoftwareVersion := dcim.NewSoftwareVersion{
+		Platform: "9a47828b-0f74-42b0-8d30-99fd9806cb1f",
+		Status:   "active",
+		Version:  "6.15.2",
+	}
+
+	gock.New(testURL).Post("dcim/software-versions/").
+		JSON(newSoftwareVersion).
+		Reply(201).
+		File(path.Join("fixtures", "dcim", "softwareversion_create_200_1.json"))
+
+	resp, err := testClient.Dcim.SoftwareVersionCreate(newSoftwareVersion)
+	require.NoError(t, err)
+	assert.Equal(t, newSoftwareVersion.Version, resp.Version)
+	assert.Equal(t, uuid.MustParse(testSoftwareVersionID), resp.ID)
+}
+
+func TestClient_SoftwareVersionUpdate(t *testing.T) {
+	updateData := map[string]any{
+		"version": "6.15.3",
+	}
+
+	gock.New(testURL).Patch("dcim/software-versions/" + testSoftwareVersionID + "/").
+		JSON(updateData).
+		Reply(200).
+		File(path.Join("fixtures", "dcim", "softwareversion_update_200_1.json"))
+
+	id, err := uuid.Parse(testSoftwareVersionID)
+	require.NoError(t, err)
+
+	resp, err := testClient.Dcim.SoftwareVersionUpdate(id, updateData)
+	require.NoError(t, err)
+
+	assert.Equal(t, "6.15.2", resp.Version)
+	assert.Equal(t, id, resp.ID)
+}
+
+func TestClient_SoftwareVersionDelete(t *testing.T) {
+	gock.New(testURL).Delete("dcim/software-versions/" + testSoftwareVersionID + "/").Reply(204)
+
+	id, err := uuid.Parse(testSoftwareVersionID)
+	require.NoError(t, err)
+
+	err = testClient.Dcim.SoftwareVersionDelete(id)
+	require.NoError(t, err)
+	assert.True(t, gock.IsDone())
+}
